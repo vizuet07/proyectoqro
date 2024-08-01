794e624b8bfaefe92c197bf05bdc73ac0c129c3b
DogeMX12 <79465956+DogeMX12@users.noreply.github.com>
2024-08-01 17:27:14 -0600

    poncho8/1/24 V2

--- config/database.php
+++ config/database.php
@@ -53,7 +53,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => env('DB_ENGINE', 'InnoDB'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = "-06:00"',
